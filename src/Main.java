e89d56044f63744f84cf634cf363b48b730918f5
Undernoone <undernoone@qq.com>
2024-01-30 18:44:53 +0800

    20240130

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+/**
+ * @since 0130
+ * @author Undernoone
+ */
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("HelloWorld");
+//        int a = 10;
+//        System.out.println(a);
+//        a = 20;
+//        System.out.println(a);
+//        short a = 10;
+//        int b = a;
+//        System.out.println(a);
+//        System.out.println(b);
+//        int c = 10;
+//        short d =c;
+//        System.out.println(c);
+//        System.out.println(d);
+//大能兼容小，小不能兼容大
+//        final int a = 10;
+//        a =100;
+//        System.out.println(0xA);
+//        System.out.println(012);
+//        String str = "长春理工大学" + "NnderNoOne"+true+'A';
+//        System.out.println(str);
+//new Person();
+//            Person person = new Person();
+//            person.name ="小明";
+//            person.age = 18;
+//            person.sex = "男";
+//            System.out.println(person.name);
+//            Person p1 = new Person();
+//            Person p2 = p1;
+//        p1.name ="小明";
+//        System.out.println(p1 == p2);
+//        System.out.println(p1.name);
+//        Person p = null;
+//        p.name="小明";
+//        System.out.println(p.name);
+// 类不需要main
+}
+}
