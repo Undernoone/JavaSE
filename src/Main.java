f88c9aa7af0f0966affc0090d1a6761d80a11cdf
Undernoone <undernoone@qq.com>
2024-02-08 00:35:42 +0800

    20240130

--- src/Main.java
+++ src/Main.java
@@ -17,47 +17,49 @@ public class Main {
 //        short d =c;
 //        System.out.println(c);
 //        System.out.println(d);
-//大能兼容小，小不能兼容大
+//        大能兼容小，小不能兼容大
 //        final int a = 10;
 //        a =100;
 //        System.out.println(0xA);
 //        System.out.println(012);
 //        String str = "长春理工大学" + "NnderNoOne"+true+'A';
 //        System.out.println(str);
-//new Person();
-//            Person person = new Person();
-//            person.name ="小明";
-//            person.age = 18;
-//            person.sex = "男";
-//            System.out.println(person.name);
-//            Person p1 = new Person();
-//            Person p2 = p1;
+//        new Person();
+//        Person person = new Person();
+//        person.name ="小明";
+//        person.age = 18;
+//        person.sex = "男";
+//        System.out.println(person.name);
+//        Person p1 = new Person();
+//        Person p2 = p1;
 //        p1.name ="小明";
 //        System.out.println(p1 == p2);
 //        System.out.println(p1.name);
 //        Person p = null;
 //        p.name="小明";
 //        System.out.println(p.name);
-// 类不需要main
-//    Person person = new Person();
-//    person.name = "王义";
-//    person.age = 18;
-//    person.hello();
+//        类不需要main
+//        Person person = new Person();
+//        person.name = "王义";
+//        person.age = 18;
+//        person.hello();
 //        int result = person.sum(10,20);
 //        System.out.println(result);
 //        Person p = new Person();
-////        int a = 5;
-////        int b =10;
+//        int a = 5;
+//        int b =10;
 //        p.swap(a,b);
 //        System.out.println(a);
 //        System.out.println(b);
-//
-
-
-
-
-
-
+//        Person person = new Person();
+//        person.setName("undernoone");
+//        System.out.println(person.name);
+//        Person person = new Person();
+//        System.out.println(person.sum(1.4,2.4));
+//        Person person = new Person();
+//        System.out.println(person.sum(1,2));
+//        Person person = new Person();这句话就是在构造Person这个方法
+//        Person person = new Person("undernone",20,"男");
 
 
 
--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,7 @@
 public class Person {
-//        String name;
-//        int age;
-//        String sex;
+        String name;
+        int age;
+        String sex;
 //
 //        void hello(){
 //                System.out.println("我叫"+name+","+"今年"+age+"岁");
@@ -10,10 +10,31 @@ public class Person {
 //                int c = a+b;
 //                return c;
 //        }
-        void swap(int a , int b){
-                 int temp = b;
-                 b = a;
-                 a = temp;
+//        void swap(int a , int b){
+//                 int temp = b;
+//                 b = a;
+//                 a = temp;
+//        }
+//        void setName(String name){
+//            this.name = name;
+////        }
+//        public String getName() {
+//                return this.name;
+////        }
+//        int sum(int a, int b){
+//                return  a+b;
+//        }
+//        double sum(double a,double b){
+//                return  a+b;
+//        }
+//
+        Person(String name;int age;String sex;){
+                this.name=name;
+                this.age=age;
+                this.sex=sex;
+        }
+        Person(){
+
         }
 
 
